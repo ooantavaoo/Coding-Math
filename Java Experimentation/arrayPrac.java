fe7870bd9b87a388e5db5ea8606adb7fac37a77d
Arrjuna P <105677807+ooantavaoo@users.noreply.github.com>
2026-01-07 17:48:47 -0500

    ?
    nn

--- Java Experimentation/arrayPrac.java
+++ Java Experimentation/arrayPrac.java
@@ -19,32 +19,22 @@ public class arrayPrac {
         final int[] array1 = { 3, 2, 4, 5, 67, 1 };
         int array1_length = Array.getLength(array1);
         int i = 1;
+        int i2 = 0;
         int index = 0;
-        int booleanCount = 0;
+        int[] array2 = new int[array1_length - 1];
 
-        int[] array2 = new int[array1_length];
-        while (index < array1_length) {
-            if (array1[index] < array1[i] && i < array1_length && index < array1_length) {
-                booleanCount++;
+        while (i < array1_length) {
+            if (array1[index] < array1[i]) {
+                array2[i2] = array1[i];
                 i++;
-            } else if (array1[index] > array1[i]) {
-                i++;
-            } else if ((Integer) array1[index] == (Integer) array1[i]) {
-                i++;
-            } else if (index == array1_length){
-                break;
+                i2 ++;
+
+            } else {
+                index = i;
             }
 
-            else {
-                System.out.println(array1_length - booleanCount);
-                Array.set(array2, (array1_length - booleanCount), array1[index]);
-                index++;
-                i++;
-                booleanCount = 0;
-
-            }
-
+            System.out.println(Arrays.toString(array2));
         }
-        System.out.println(Arrays.toString(array2));
+
     }
 }
